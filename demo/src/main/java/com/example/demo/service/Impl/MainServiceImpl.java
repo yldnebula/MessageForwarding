60ee05cabde315a8fb6133d27c3a54b1593cdb43
yldnebula <yldnebula@foxmail.com>
2022-08-08 08:59:58 +0800

    暂存

--- demo/src/main/java/com/example/demo/service/Impl/MainServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/Impl/MainServiceImpl.java
@@ -5,39 +5,47 @@ import com.example.demo.Event.MessageEvent;
 import com.example.demo.controller.SocketServerController;
 import com.example.demo.service.MainService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Scope;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
+import java.util.AbstractMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
 public class MainServiceImpl implements MainService {
-    private CountDownLatch countDownLatch;
-    private Message message;
+//    private CountDownLatch countDownLatch;
+//    private Message send;
+//    private Message recv;
+    private static final ConcurrentHashMap<Map.Entry<String, String>, Map.Entry<CountDownLatch, Message>> requestMap = new ConcurrentHashMap<>();
     @Override
-    public Message sendSyncMessage(Message message){
-        countDownLatch = new CountDownLatch(1);
-        final boolean[] isTimeOut = {false};
+    public Message sendSyncMessage(Message message) {
+        String source = message.getSource();
+        String target = message.getTarget();
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        AbstractMap.SimpleEntry<String, String> stringStringSimpleEntry = new AbstractMap.SimpleEntry<>(source, target);
+        requestMap.put(stringStringSimpleEntry, new AbstractMap.SimpleEntry<>(countDownLatch, null));
         SocketServerController.sendMessage(message);
         new Thread(() -> {
             try {
-                Thread.sleep(30000);
-                isTimeOut[0] = true;
-                countDownLatch.countDown();
-                //TODO
-                //如果刚好阻塞完成，这时候收到event。。还是当作超时处理吗？
+                Thread.sleep(31000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }).start();
+        boolean await = false;
         try {
-            countDownLatch.await();
+            await = countDownLatch.await(30, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return isTimeOut[0]?null:this.message;
+        Message value = requestMap.get(stringStringSimpleEntry).getValue();
+        return await ? value : null;
     }
 
     @Override
@@ -53,8 +61,14 @@ public class MainServiceImpl implements MainService {
     @EventListener({MessageEvent.class})
     public void onEvent(MessageEvent event) {
         Message message = event.getMessage();
-        log.info("同步回复：{}", message);
-        this.message = message;
-        countDownLatch.countDown();
+        String source = message.getSource();
+        String target = message.getTarget();
+        AbstractMap.SimpleEntry<String, String> stringStringSimpleEntry = new AbstractMap.SimpleEntry<>(target, source);
+        Map.Entry<CountDownLatch, Message> countDownLatchMessageEntry = requestMap.get(stringStringSimpleEntry);
+        if(countDownLatchMessageEntry!=null){
+            log.info("同步回复：{}", message);
+            countDownLatchMessageEntry.setValue(message);
+            countDownLatchMessageEntry.getKey().countDown();
+        }
     }
 }
